4accebaf19b0bbe26a2bdb3ce7988c3a8895283a
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-10 01:04:04 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 15}
+    validates :summary, length: {maximum: 25}
+    validates :category, inclusion: {in: %w[Fiction Non-Fiction]}
+
 end
